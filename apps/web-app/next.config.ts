f1254591e6cb3262450d4f8f59b5f76e97d5c21e
Thailan Ramos <thailanrs@gmail.com>
2026-06-23 20:33:21 -0300

    feat(config): adicionar configuração experimental para turbo no Next.js

--- apps/web-app/next.config.ts
+++ apps/web-app/next.config.ts
@@ -1,7 +1,13 @@
 import type { NextConfig } from "next";
+import path from "path";
 
 const nextConfig: NextConfig = {
   transpilePackages: ["@pipevitta/database", "@pipevitta/design-system"],
+  experimental: {
+    turbo: {
+      root: path.resolve(__dirname),
+    },
+  },
 };
 
 export default nextConfig;
